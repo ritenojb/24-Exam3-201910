227c2ffc6a5d73993d913572d175f0d39f7b40f8
ritenojb <ritenojb@rose-hulman.edu>
2018-10-25 22:15:34 -0400

    Problem 5 done

--- src/problem5.py
+++ src/problem5.py
@@ -2,9 +2,9 @@
 Final exam, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Jacob Ritenour.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -216,6 +216,14 @@ def problem5(x):
     # TODO: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    smallest_prime = 0
+    while(True):
+        if(is_prime(x)==True):
+            if(is_prime(sum_of_digits(x))==True):
+                smallest_prime = x
+                break
+        x=x+1
+    return smallest_prime
 
 
 ###############################################################################
